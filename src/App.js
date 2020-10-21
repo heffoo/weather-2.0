75ff3d8dbdd200b5c47a62d1fe7fce1447f3e90b
heffoo <57847291+heffoo@users.noreply.github.com>
2020-10-21 13:55:32 +0300

    weather is showing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import config from "./config";
 
 import "./App.css";
 
 function App() {
   let configs = config;
-
+  const [weather, setWeather] = useState();
   let params = {
-    APPID: "43da68da51e7243f3d7714e461f6e74f",
+    APPID: "5f892c8a0b4c47ee1b455fa5bbc9851f",
   };
-  params = { ...params, q: "Moscow" };
-
+  params = { ...params, q: "Krasnodar" };
 
   const esc = encodeURIComponent;
   let query =
@@ -22,31 +21,63 @@ function App() {
   console.log(params);
   // `${document.getElementById("input").value}`
 
-let weather;
+  useEffect(() => {
+    async function test() {
+      await fetcha();
+    }
+    test();
+  }, []);
 
+  async function fetcha() {
+    const handler = await fetch(`${configs.apiUrl}${query}`);
+    let response = await handler.json();
+    setWeather(response);
 
-  fetch(`${configs.apiUrl}${query}`)
-    .then((response) => response.json())
-    .then((res) => weather = res)
-    .catch((err) => {
-      console.error(err);
-    });
-
-  console.log('weather', weather);
-  //  function QueryParams(params) {
-  //   return "?city={lat}&lang={lon}&appid={apikey}";
-  //  };
-
+    // .then((response) => response.json())
+    // .then((response) => (weather = response))
+    // .catch((err) => {
+    //   console.error(err);
+    // });
+  }
+  console.log(weather);
   return (
     <div className="App">
       <div className="main-container">
         <input type="text" id="input"></input>
+        <div className="smth">
+         широта {weather && weather.coord.lon}
+         долгота {weather && weather.coord.lat}
+        градусы {weather && weather.main.temp}
+        
+        </div>
+        
       </div>
     </div>
   );
 }
 
 export default App;
+
+// ---------------------------------------------------------
+
+//   useEffect(() => {
+//     fetchQuotes()
+
+// }, [weathercity])
+
+// const fetchQuotes = () => {
+//   setFething(true)
+//   fetch(`${configs.apiUrl}${query}`)
+//       .then((response) => response.json())
+//       .then(res => console.log(res))
+//       .then(res => setWeather(res))
+//       .then(setFething(false))
+//       .catch((err) => {
+//         console.error(err);
+//       });
+// }
+
+// ---------------------------------------------------------
 
 // config = {
 //   apikey: huigrehghehips57887
