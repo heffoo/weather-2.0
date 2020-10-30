358c048059abcc92d91be05a1194b14a69570c12
heffoo <57847291+heffoo@users.noreply.github.com>
2020-10-30 16:21:14 +0300

    Build app

---
+++ docs/precache-manifest.129368a3f0bec5d2e03fe810011e4a4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecb2f49388f14e633956cbc9db78f25e",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "12bf78e21d4642136bbb",
+    "url": "/weather/static/css/main.159584ee.chunk.css"
+  },
+  {
+    "revision": "bd068923dc421f7fa6ac",
+    "url": "/weather/static/js/2.24ad60a2.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/weather/static/js/2.24ad60a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12bf78e21d4642136bbb",
+    "url": "/weather/static/js/main.17db4bc7.chunk.js"
+  },
+  {
+    "revision": "65a135568c88d392bf25",
+    "url": "/weather/static/js/runtime-main.d8725fa2.js"
+  }
+]);
